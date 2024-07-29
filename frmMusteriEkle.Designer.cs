a21b8a6f8cd1f1e0ecb96b8a985dc360aad9a549
Win11 <Win11@DESKTOP-FOREST>
2024-07-29 14:32:44 +0300

    Proje dosyası ekle.

---
+++ frmMusteriEkle.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace StokTakipOtomasyonu
+{
+    partial class frmMusteriEkle
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txttckimlik = new System.Windows.Forms.TextBox();
+            this.txtadsoyad = new System.Windows.Forms.TextBox();
+            this.txttelefon = new System.Windows.Forms.TextBox();
+            this.txtadres = new System.Windows.Forms.TextBox();
+            this.txteposta = new System.Windows.Forms.TextBox();
+            this.btnekle = new System.Windows.Forms.Button();
+            this.btntemizle = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Yu Gothic UI Semilight", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(40, 64);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(244, 50);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "T.C. Kimlik No:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Yu Gothic UI Semilight", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(40, 151);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(187, 50);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Ad-Soyad:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Yu Gothic UI Semilight", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(40, 238);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(143, 50);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Telefon:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Yu Gothic UI Semilight", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(40, 325);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(122, 50);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Adres:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Yu Gothic UI Semilight", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(40, 412);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(150, 50);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "E-Posta:";
+            // 
+            // txttckimlik
+            // 
+            this.txttckimlik.Font = new System.Drawing.Font("Yu Gothic", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txttckimlik.Location = new System.Drawing.Point(307, 60);
+            this.txttckimlik.Name = "txttckimlik";
+            this.txttckimlik.Size = new System.Drawing.Size(430, 54);
+            this.txttckimlik.TabIndex = 5;
+            // 
+            // txtadsoyad
+            // 
+            this.txtadsoyad.Font = new System.Drawing.Font("Yu Gothic", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtadsoyad.Location = new System.Drawing.Point(307, 147);
+            this.txtadsoyad.Name = "txtadsoyad";
+            this.txtadsoyad.Size = new System.Drawing.Size(430, 54);
+            this.txtadsoyad.TabIndex = 6;
+            // 
+            // txttelefon
+            // 
+            this.txttelefon.Font = new System.Drawing.Font("Yu Gothic", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txttelefon.Location = new System.Drawing.Point(307, 234);
+            this.txttelefon.Name = "txttelefon";
+            this.txttelefon.Size = new System.Drawing.Size(430, 54);
+            this.txttelefon.TabIndex = 7;
+            // 
+            // txtadres
+            // 
+            this.txtadres.Font = new System.Drawing.Font("Yu Gothic", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtadres.Location = new System.Drawing.Point(307, 321);
+            this.txtadres.Name = "txtadres";
+            this.txtadres.Size = new System.Drawing.Size(430, 54);
+            this.txtadres.TabIndex = 8;
+            // 
+            // txteposta
+            // 
+            this.txteposta.Font = new System.Drawing.Font("Yu Gothic", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txteposta.Location = new System.Drawing.Point(307, 408);
+            this.txteposta.Name = "txteposta";
+            this.txteposta.Size = new System.Drawing.Size(430, 54);
+            this.txteposta.TabIndex = 9;
+            // 
+            // btnekle
+            // 
+            this.btnekle.BackColor = System.Drawing.Color.Silver;
+            this.btnekle.Font = new System.Drawing.Font("Yu Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnekle.ForeColor = System.Drawing.Color.SeaShell;
+            this.btnekle.Location = new System.Drawing.Point(307, 516);
+            this.btnekle.Name = "btnekle";
+            this.btnekle.Size = new System.Drawing.Size(190, 43);
+            this.btnekle.TabIndex = 10;
+            this.btnekle.Text = "Ekle";
+            this.btnekle.UseVisualStyleBackColor = false;
+            this.btnekle.Click += new System.EventHandler(this.btnekle_Click);
+            // 
+            // btntemizle
+            // 
+            this.btntemizle.BackColor = System.Drawing.Color.Silver;
+            this.btntemizle.Font = new System.Drawing.Font("Yu Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btntemizle.ForeColor = System.Drawing.Color.SeaShell;
+            this.btntemizle.Location = new System.Drawing.Point(547, 516);
+            this.btntemizle.Name = "btntemizle";
+            this.btntemizle.Size = new System.Drawing.Size(190, 43);
+            this.btntemizle.TabIndex = 11;
+            this.btntemizle.Text = "Temizle";
+            this.btntemizle.UseVisualStyleBackColor = false;
+            this.btntemizle.Click += new System.EventHandler(this.btntemizle_Click);
+            // 
+            // frmMusteriEkle
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SeaShell;
+            this.ClientSize = new System.Drawing.Size(804, 656);
+            this.Controls.Add(this.btntemizle);
+            this.Controls.Add(this.btnekle);
+            this.Controls.Add(this.txteposta);
+            this.Controls.Add(this.txtadres);
+            this.Controls.Add(this.txttelefon);
+            this.Controls.Add(this.txtadsoyad);
+            this.Controls.Add(this.txttckimlik);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "frmMusteriEkle";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Müşteri Ekleme Sayfası";
+            this.Load += new System.EventHandler(this.frmMusteriEkle_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txttckimlik;
+        private System.Windows.Forms.TextBox txtadsoyad;
+        private System.Windows.Forms.TextBox txttelefon;
+        private System.Windows.Forms.TextBox txtadres;
+        private System.Windows.Forms.TextBox txteposta;
+        private System.Windows.Forms.Button btnekle;
+        private System.Windows.Forms.Button btntemizle;
+    }
+}
---
+++ frmsatissayfa.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StokTakipOtomasyonu
+{
+    public partial class frmsatissayfa : Form
+    {
+        public frmsatissayfa()
+        {
+            InitializeComponent();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void btnmusteriekle_Click(object sender, EventArgs e)
+        {
+            frmMusteriEkle ekle = new frmMusteriEkle();
+            ekle.ShowDialog();
+        }
+
+        private void btnmusterilistele_Click(object sender, EventArgs e)
+        {
+            frmmusterilistele listele = new frmmusterilistele();
+            listele.ShowDialog();
+        }
+    }
+}
